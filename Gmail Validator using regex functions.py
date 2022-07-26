a4661945ef6bdd98b1c15dad5bf140b2600d9d31
Shivam Kumar <shivambharadwaj822@gmail.com>
2022-07-26 12:20:08 +0530

    Update Gmail Validator using regex functions.py

--- Gmail Validator using regex functions.py
+++ Gmail Validator using regex functions.py
@@ -1,8 +1,8 @@
-import re
-gmail_condition = "^[a-z]+[a-z 0-9]+[\._]?[@]\w+[.]\w{2,3}$"
-enter_gmail = input("Enter your Gmail : ") #enter your gmail
+import re #importing function
+gmail_condition = "^[a-z]+[a-z 0-9]+[\._]?[@]\w+[.]\w{2,3}$" #all valid condition for correct gmail format
+enter_gmail = input("Enter your Gmail : ") #enter your gmail for checking
 
-if re.search(gmail_condition,enter_gmail):
+if re.search(gmail_condition,enter_gmail): 
     print("Right Gmail !")
 else:
     print("Wrong Gmail !")
